9881307465fe3690f76792ca1f437a69c130dfd9
ArchLeaders <archleadership28@gmail.com>
2024-04-01 22:21:28 -0700

    Impl hash table name resolver

---
+++ src/AampLibrary/IO/Hashing/HashTable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Frozen;
+
+namespace AampLibrary.IO.Hashing;
+
+public class HashTable : IAampNameResolver
+{
+    private readonly FrozenDictionary<uint, string> _lookup;
+
+    public HashTable(IDictionary<uint, string> values)
+    {
+        _lookup = FrozenDictionary.ToFrozenDictionary(values);
+    }
+
+    public HashTable(FrozenDictionary<uint, string> values)
+    {
+        _lookup = values;
+    }
+
+    public string GetName(uint hash)
+    {
+        return _lookup[hash];
+    }
+}
