ceac42eb7b6a383046ebae38e29ebf69825be3a0
ArchLeaders <archleadership28@gmail.com>
2024-07-30 23:03:11 -0700

    Use fixed array instead of ptr

--- src/AampLibrary/Primitives/Curve.cs
+++ src/AampLibrary/Primitives/Curve.cs
@@ -2,19 +2,8 @@
 
 namespace AampLibrary.Primitives;
 
-[StructLayout(LayoutKind.Sequential, Size = 0x80)]
 public unsafe struct Curve
 {
-    private const int FLOAT_COUNT = 30;
-
     public uint A, B;
-    private readonly float _float0;
-
-    public readonly Span<byte> Floats {
-        get {
-            fixed (float* ptr = &_float0) {
-                return new(ptr, 30);
-            }
-        }
-    }
+    public fixed float Points[30];
 }
