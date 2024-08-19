762312cc51a0d6750f6de56e9dedc47c1df4aaf6
ArchLeaders <archleadership28@gmail.com>
2024-08-19 10:19:56 -0700

    Update debug runner

--- src/AampLibrary.Runner/Program.cs
+++ src/AampLibrary.Runner/Program.cs
@@ -1,42 +1,10 @@
 ﻿using AampLibrary;
-using AampLibrary.IO;
-using AampLibrary.Runner.Helpers;
-using AampLibrary.Yaml;
-using System.Text;
+using Revrs;
 
-AampKeyProvider keyProvider = new([
-    "TestObject",
-    "ListObject",
-    "TestObject2",
-    "Bool",
-    "Float",
-    "Int",
-    "Vec2",
-    "Vec3",
-    "Vec4",
-    "Color",
-    "String32",
-    "String64",
-    "Curve1",
-    "Curve2",
-    "Curve3",
-    "Curve4",
-    "IntArray",
-    "FloatArray",
-    "String256",
-    "Quat",
-    "UInt32",
-    "UInt32Array",
-    "ByteArray",
-    "StringRef",
-]);
+RevrsReader reader = new(File.ReadAllBytes(
+    @"D:\bin\AampLibrary\Armor_006_Upper.bphysics"
+));
+ImmutableAamp aampView = new(ref reader);
 
-Aamp aamp = TestAampGenerator.CreateAamp();
-string yaml = aamp.ToYaml(keyProvider);
-
-// Console.WriteLine(yaml);
-
-byte[] utf8Yaml = Encoding.UTF8.GetBytes(yaml);
-Aamp fromYaml = AampYamlParser.FromYaml(new(utf8Yaml));
-
-Console.WriteLine(fromYaml.ToYaml(keyProvider));
+Aamp aamp = Aamp.FromImmutable(ref aampView);
+// write binary
